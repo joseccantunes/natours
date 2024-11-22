0785205f0ff2dc5dfbf20200ba4affc00da11cc3
Jose Antunes <joseccantunes@gmail.com>
2024-11-22 22:58:01 +0000

    sort requires so app get access env variables

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require('dotenv').config();
 const app = require('./app');
-
-dotenv.config();
 
 mongoose
   .connect(process.env.DATABASE)
