8a70628a07d0244da309ecf9edb3217d3ecec9f3
Jose Antunes <joseccantunes@gmail.com>
2024-11-24 09:11:28 +0000

    Added document middleware to manipulate documents that are currently being saved (lesson 105)

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -36,7 +38,7 @@ const tourSchema = new mongoose.Schema(
     summary: {
       type: String,
       trim: true,
-      required: [true, 'A tour must have a description'],
+      required: [true, 'A tour must have a summary'],
     },
     description: {
       type: String,
@@ -63,6 +65,17 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+//THIS IS DOCUMENT MIDDLEWARE: runs before .save() and .created()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(thisdoc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
