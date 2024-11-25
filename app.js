2cbfb972004ec89ced7c9c3904decadee4070c38
Jose Antunes <joseccantunes@gmail.com>
2024-11-25 18:35:40 +0000

    added a Global Error handling (lesson 114)

--- app.js
+++ app.js
@@ -28,11 +28,19 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, resp, next) => {
-  resp.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, resp, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  resp.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
-  next();
 });
 
 module.exports = app;
