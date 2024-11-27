0070a9265fc737e552f8ae3633a234943f4a88cf
Jose Antunes <jose.antunes@dataharvesting.com>
2024-11-27 23:05:00 +0000

    updated get AllUsers function so we can test it

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,16 @@
-exports.getAllUsers = (req, resp) => {
-  resp.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, resp, next) => {
+  const users = await User.find();
+  resp.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 exports.createUser = (req, resp) => {
   resp.status(500).json({
