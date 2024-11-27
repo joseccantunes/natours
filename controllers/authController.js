4a450b16fed9cfc88add10a6cf6512e1da1049cf
Jose Antunes <jose.antunes@dataharvesting.com>
2024-11-27 23:06:57 +0000

    added login function to handle login route added password validation and send token back

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+function signToken(id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+}
 
 exports.signup = catchAsync(async (req, resp, next) => {
   const newUser = await User.create({
@@ -11,9 +18,7 @@ exports.signup = catchAsync(async (req, resp, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   resp.status(201).json({
     status: 'success',
@@ -21,5 +26,29 @@ exports.signup = catchAsync(async (req, resp, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, resp, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  // 2) Check if the user exists && password is correct
+  //+password -> gets the password... Password was disabled in the schema by using select: false
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.validatePassword(password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3) if everything is ok, send token to client
+  const token = signToken(user._id);
+  resp.status(200).json({
+    status: 'success',
+    token,
   });
 });
