fe4056db389989a1aaf076ef0ca9c66cac606a9a
Jose Antunes <jose.antunes@dataharvesting.com>
2024-11-30 15:12:52 +0000

    updated token sender to send secure token (142)

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,17 @@ function signToken(id) {
 function createSendToken(user, statusCode, resp) {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  resp.cookie('jwt', token, cookieOptions);
+
   //just in case password is set (i.e. login, changePassword), let's unset it
   user.password = undefined;
 
