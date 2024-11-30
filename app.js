e5e5979c919adfbe1f5afe0bf6e4006b760f320b
Jose Antunes <jose.antunes@dataharvesting.com>
2024-11-30 15:38:18 +0000

    added helmet to app and added some comments to code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const morgan = require('morgan');
 
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,11 +12,17 @@ const AppError = require('./utils/appError');
 const app = express();
 
 // 1. GLOBAL MIDDLEWARES
+
+//Se security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   // Middleware to log requests
   app.use(morgan('dev'));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000, //1h
@@ -24,18 +31,19 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-// Middleware to parse JSON bodies
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, resp, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 // 2. ROUTE HANDLERS
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
