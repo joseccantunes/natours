93c72b44c417962e7728e4b0f1ac80f9df7af7f5
Jose Antunes <jose.antunes@dataharvesting.com>
2024-11-30 15:29:14 +0000

    added rate limit to the global middleware (144)

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
+
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
 
 // Initializing the express application
 const app = express();
 
-// 1. MIDDLEWARES
+// 1. GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   // Middleware to log requests
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, //1h
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+
+app.use('/api', limiter);
 
 // Middleware to parse JSON bodies
 app.use(express.json());
