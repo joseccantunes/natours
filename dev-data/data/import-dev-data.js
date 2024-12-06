4ed02042ed688aa521748cacea269f81dd276925
Jose Antunes <jose.antunes@dataharvesting.com>
2024-12-06 18:48:21 +0000

    added import users and reviews

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 
 dotenv.config();
 
+const Review = require('../../models/reviewModel');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
 
 mongoose
   .connect(process.env.DATABASE)
@@ -13,11 +15,17 @@ mongoose
 
 //READ FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
+);
 
 //IMPORT DATA INTO DATABASE
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data succesfully loaded');
   } catch (err) {
     console.log(err);
@@ -29,6 +37,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data succesfully deleted');
   } catch (err) {
     console.log(err);
